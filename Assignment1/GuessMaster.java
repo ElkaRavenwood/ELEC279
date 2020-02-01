11622f38de68e219f88d3ff07f261c716bd8f96e
Jamie Won <jamie.won.2000@gmail.com>
2020-02-01 17:52:42 -0500

    Fused classes

--- Assignment1/GuessMaster.java
+++ Assignment1/GuessMaster.java
@@ -148,4 +148,25 @@ public class GuessMaster {
 		return this.entities[r.nextInt(this.numberOfCandidateEntities - 1)];
 
 	}
+
+	// main method
+	public static void main (String [] args) {
+
+		// test entities
+		Entity trudeau = new Entity("Justin Trudeau", new Date("December", 25, 1971));
+		Entity dion = new Entity("Celine Dion", new Date("March", 30, 1968));
+		Entity usa = new Entity("United States", new Date("July", 4, 1776));
+
+		// creates GuessMaster and adds entities
+		GuessMaster gm = new GuessMaster();
+		gm.addEntity(trudeau);
+		gm.addEntity(dion);
+		gm.addEntity(usa);
+
+		// plays game
+		gm.playGame();
+		// gm.playGame(trudeau);
+		// gm.playGame(2);
+
+	}
 }
