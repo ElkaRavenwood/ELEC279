5e393114886a3c9fcc3e541d10434d9717fa749b
Jamie Won <jamie.won.2000@gmail.com>
2020-02-19 23:45:27 -0500

    Part 1 - Student class

---
+++ Lab3/Student.java
@@ -0,0 +1,58 @@
+/*
+ * Author: Jamie Won
+ * Date: February 14, 2020
+ * Lab 2: Part 1
+ */
+
+public class Student extends Person {
+
+	// class variables
+	private int courses;
+	private String[] codes;
+	private int[] grades;
+	static final int maxCourses = 20;
+
+	/** GradStudent
+	 * constructor
+	 * @param name the student's name
+	 * @param address the student's address
+	 * @param id the student's student id number
+	 */
+	public Student (String name, String address, int id) {
+		super(name, address, id);
+		courses = 0;
+		codes = new String [maxCourses];
+		grades = new int [maxCourses];
+	}
+
+	/** toString
+	 * Overrides the person's toString
+	 * @return the new string to be returned, marks as a student
+	 */
+	public String toString () {
+		return "Student: " + super.toString();
+	}
+
+	/** addGrade
+	 * Adds a grade to the GradStudent
+	 * @param code the course code
+	 * @param grade the grade of the course 
+	 */
+	public void addGrade (String code, int grade) {
+		
+		if (courses != maxCourses) { // if less than max courses are being taken
+			codes[courses] = code; // adds course
+			grades[courses] = grade; // adds grade
+			courses++; // increments courses taken
+		}
+
+	}
+
+	/** printExamGrades
+	 * prints the course  grades
+	 */
+	public void printGrades () {
+		for (int i = 0; i < courses; i ++) System.out.println(codes[i] + ":" + grades[i]);		
+	}
+
+}
