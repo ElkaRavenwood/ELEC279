1ebef3239797ce81acab5703935162b14bf280da
Jamie Won <jamie.won.2000@gmail.com>
2020-02-19 23:45:53 -0500

    Part 2 - Grad Student class

---
+++ Lab3/GradStudent.java
@@ -0,0 +1,71 @@
+/**
+ * @author Jamie Won
+ * Date: February 14, 2020
+ * Lab 2: Part 2
+ */
+
+public class GradStudent extends Student {
+	
+	// class variables
+	private int comitteeNum;
+	private int examsTaken;
+	private String[] examNames;
+	private String[] examGrades;
+	private final int maxExams = 6;
+
+	/** GradStudent
+	 * constructor
+	 * @param name the student's name
+	 * @param address the student's address
+	 * @param id the student's student id number
+	 */
+	public GradStudent (String name, String address, int id) {
+		super(name, address, id);
+		comitteeNum = 0;
+		examsTaken = 0;
+		examNames = new String[maxExams];
+		examGrades = new String[maxExams];
+	}
+
+	/** getComitteeNum
+	 * @return the current comittee num
+	 */
+	public int getComitteeNum () {
+		return comitteeNum;
+	}
+
+	/** getComitteeNum
+	 * @param num new committee num
+	 */
+	public void setComitteeNum (int num) {
+		comitteeNum = num;
+	}
+
+	/** toString
+	 * Overrides the Student's toString
+	 * @return the new string to be returned, marks as a grad student
+	 */
+	public String toString () {
+		return "Grad " + super.toString();
+	}
+
+	/** addGrade
+	 * Adds a grade to the GradStudent
+	 * @param exam the name of the exam written
+	 * @param grade the grade of the exam (pass/fail)
+	 */
+	public void addGrade (String exam, String grade) {
+		examNames[examsTaken] = exam;
+		examGrades[examsTaken] = grade;
+		examsTaken++;
+	}
+
+	/** printExamGrades
+	 * prints the grad level course exam grades
+	 */
+	public void printExamGrades() {
+		for (int i = 0; i < examsTaken; i ++) System.out.println(examNames[i] + ":" + examGrades[i]);
+	}
+
+
+}
