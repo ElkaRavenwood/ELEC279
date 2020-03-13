f6d44bbef0cddd55c142242f65fdd14a29c5741b
Jamie Won <jamie.won.2000@gmail.com>
2020-03-13 09:02:13 -0400

    Part 2

---
+++ Lab4/Invoice.java
@@ -0,0 +1,70 @@
+public class Invoice implements Comparable<Invoice>{
+	
+	private String companyName;
+	private Double amount;
+	
+	public Invoice(){
+		companyName = "No Name";
+		amount = 0.0;
+	}
+	
+	public Invoice(String theName, Double theAmount){
+		companyName = theName;
+		if (theName == null || theAmount < 0)
+        {
+             System.out.println("Fatal Error creating Invoice.");
+             System.exit(0);
+        }
+        companyName = theName;
+        amount = theAmount;
+	}
+	
+	public String getName(){
+		return companyName;
+	}
+	
+	public Double getAmount(){
+		return amount;
+	}
+	
+	public void setName(String newName){
+		if (newName == null)
+        {
+             System.out.println("Fatal Error setting invoice name.");
+             System.exit(0);
+        }
+       else
+            companyName = newName;
+	}
+	
+	public void setAmount(Double newAmount){
+		if (newAmount < 0)
+        {
+             System.out.println("Fatal Error setting invoice name.");
+             System.exit(0);
+        }
+       else
+            amount = newAmount;
+	}
+	
+	public String toString(){
+		return companyName + " is owed " + amount;
+	}
+	
+	/**
+	 * Implementation of Comparable interface
+	 * @param bill the invoice this one will be compared you
+	 * @return the int representing their comparison
+	 */
+	public int compareTo(Invoice bill) {
+		// if (amount == bill.getAmount()) {
+		// 	return 0;
+		// } else if (amount < bill.getAmount()) {
+		// 	return -1;
+		// } else {
+		// 	return 1;
+		// }
+		return (int)(amount - bill.getAmount());
+	}
+
+}
--- Lab4/Lab4.java
+++ Lab4/Lab4.java
@@ -6,38 +6,38 @@ public class Lab4 {
 	public static void main(String[] args)
     {
 		// Part 1
-        HourlyEmployee[] staff = new HourlyEmployee[5];
-        Date theDate = new Date(2005-1900,1,1);
-        for(int i = 0; i < 5; i++){
-        	theDate.setYear(2005+i-1900);
-        	staff[i] = new HourlyEmployee("Alice" + (5-i),(Date)theDate.clone(), 15.0, 10+i*4.0);
-        }
+        // HourlyEmployee[] staff = new HourlyEmployee[5];
+        // Date theDate = new Date(2005-1900,1,1);
+        // for(int i = 0; i < 5; i++){
+        // 	theDate.setYear(2005+i-1900);
+        // 	staff[i] = new HourlyEmployee("Alice" + (5-i),(Date)theDate.clone(), 15.0, 10+i*4.0);
+        // }
         
-        for(HourlyEmployee temp : staff){
-        	System.out.println(temp.toString());
-        }
+        // for(HourlyEmployee temp : staff){
+        // 	System.out.println(temp.toString());
+        // }
         
-        Arrays.sort(staff);
+        // Arrays.sort(staff);
         
-        for(HourlyEmployee temp : staff){
-        	System.out.println(temp.toString());
-        }
+        // for(HourlyEmployee temp : staff){
+        // 	System.out.println(temp.toString());
+        // }
         
         // // Part 2
-        // Invoice[] bills = new Invoice[5];
-        // for(int i = 0; i < 5; i++){
-        // 	bills[i] = new Invoice("Company" + (5-i), 300.0 - 50.0*i);
-        // }
+        Invoice[] bills = new Invoice[5];
+        for(int i = 0; i < 5; i++){
+        	bills[i] = new Invoice("Company" + (5-i), 300.0 - 50.0*i);
+        }
         
-        // for(Invoice temp : bills){
-        // 	System.out.println(temp.toString());
-        // }
+        for(Invoice temp : bills){
+        	System.out.println(temp.toString());
+        }
         
-        // Arrays.sort(bills);
+        Arrays.sort(bills);
         
-        // for(Invoice temp : bills){
-        // 	System.out.println(temp.toString());
-        // }
+        for(Invoice temp : bills){
+        	System.out.println(temp.toString());
+        }
         
         // // Part 3
         // for(PayAble temp : (PayAble[])bills){
